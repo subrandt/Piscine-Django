ddcf2e1e7682d84d578681e2e8e811968d9286dd
Susanne Brandt <susanne.brandt@scub.net>
2024-03-20 17:15:57 +0100

    il faut corriger: ca marche plus quand il y a la bonne adresse

---
+++ Django_0/ex00/myawesomescript.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Checking if there is a link given as argument
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <bit.ly_link>"
+    exit 1
+fi
+
+# Checking if the given link begins with "bit.ly"
+if ! echo "$1" | grep -q "^https\?://bit\.ly"; then
+    echo "The input has to begin with: bit.ly"
+    exit 1
+fi
+
+# Adress Bit.ly to decode
+BITLY_LINK=$1
+
+# Query the Bit.ly API to obtain the JSON response
+response=$(curl -s -L -o /dev/null -w "%{url_effective}" -D - "$BITLY_LINK")
+
+# Extract the original address from the JSON response
+original_url=$(echo "$response" | grep -oP '(?<=Location: ).*' | tr -d '\r')
+
+# Display original address
+echo "Original adress: $original_url"
