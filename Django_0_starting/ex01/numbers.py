a0de0804c3cdb5061ded914a56b029f9859b7b76
subrandt <susanneimnetz@hotmail.com>
2024-04-04 13:30:16 +0200

    numbers.py

--- Django_0_starting/ex01/numbers.py
+++ Django_0_starting/ex01/numbers.py
@@ -1,13 +1,11 @@
 def display_numbers_from_file():
     try:
-        file = open("number.txt")
+        file = open("numbers.txt")
         numbers = file.read()
-        print(numbers)
+        print(numbers.replace(",", "\n"))
 
     except Exception as e:
         print(f"toto {e}")
-
-
 
 if __name__ == '__main__':
     display_numbers_from_file()
