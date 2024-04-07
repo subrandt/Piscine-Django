08dffb7919c7f3a2b11eb31c356b73180060645b
subrandt <susanneimnetz@hotmail.com>
2024-04-07 21:35:29 +0200

    debut all_in

--- Django_0_starting/ex05/all_in.py
+++ Django_0_starting/ex05/all_in.py
@@ -1,8 +1,23 @@
 import sys
 
+def get_args(arg: str):
+    states = {
+        "Oregon" : "OR",
+        "Alabama" : "AL",
+        "New Jersey": "NJ",
+        "Colorado" : "CO"
+    }
 
+    capital_cities = {
+        "OR": "Salem",
+        "AL": "Montgomery",
+        "NJ": "Trenton",
+        "CO": "Denver"
+    }
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2:
         sys.exit(1)
-    capital_city(sys.argv[1])
+
+    for arg in sys.argv:
+        get_args(arg)
