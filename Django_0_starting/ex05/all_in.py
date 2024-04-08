23c7e878d0660b019f6513b1892137eac22d8474
subrandt <susanneimnetz@hotmail.com>
2024-04-08 16:04:40 +0200

    all_in + my_sort en cours

--- Django_0_starting/ex05/all_in.py
+++ Django_0_starting/ex05/all_in.py
@@ -1,6 +1,10 @@
 import sys
 
-def get_args(arg: str):
+def use_dict(arg: str):
+    
+    if arg == "":
+        return
+    
     states = {
         "Oregon" : "OR",
         "Alabama" : "AL",
@@ -14,10 +18,53 @@ def get_args(arg: str):
         "NJ": "Trenton",
         "CO": "Denver"
     }
+    
+    # transform dictionaries to lower case for comparison
+    states_lower = {k.lower(): v for k, v in states.items()}
+    
+    arg = arg.lower()
+    
+    
+    # if it is a state
+    if arg in states_lower:
+        tmp_state = states_lower[arg]
+        print(capital_cities[tmp_state], "is the capital of", arg)
+        return
+    
+    # inversion of the dictionaries:
+    capital_to_state = {v: k for k, v in capital_cities.items()}
+    capital_to_state_lower = {v.lower(): k for k, v in capital_cities.items()}
+    abbr_to_state = {v: k for k, v in states.items()}
+    abbr_to_state_lower = {v.lower(): k for k, v in states.items()}
+    
+    #  if it is a capital
+    if arg in capital_to_state_lower:
+        tmp2 = capital_to_state_lower[arg]
+        print(arg, "is the capital of", abbr_to_state[tmp2])
+        return
+
+    else:
+        print(arg, "is neither a capital city nor a state")
+
+    
+def get_args(arg: str):
+    # try:
+        
+        print(arg)
+        
+        after_split = [arg.strip() for arg in arg.split(',')]
+        print(after_split)
+        
+        for element in after_split:
+            use_dict(element)
+        
+
+    # except Exception:
+    #     print("error")
+
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
+    if len(sys.argv) != 2:
         sys.exit(1)
-
-    for arg in sys.argv:
-        get_args(arg)
+    
+    get_args(sys.argv[1])
