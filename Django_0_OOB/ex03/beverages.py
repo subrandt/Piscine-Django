84db1a7c708d09aac1368e3bb94564a9fe3e88f8
subrandt <susanneimnetz@hotmail.com>
2024-04-15 17:43:24 +0200

    modifs beverages et machine.py

--- Django_0_OOB/ex03/beverages.py
+++ Django_0_OOB/ex03/beverages.py
@@ -1,38 +1,36 @@
 class HotBeverage:
-    price = 0.30
-    name = "hot beverage"
+    def __init__(self, name="hot beverage", price=0.30):
+        self.name = name
+        self.price = price
 
     def description(self):
         return "Just some hot water in a cup."
     
     def __str__(self):
-        return self.description()
-
+        return f"name : {self.name}\nprice : {self.price:.2f}\ndescription : {self.description()}"
 
 class Coffee(HotBeverage):
-    name = "coffee"
-    price = 0.40
-
+    def __init__(self):
+        super().__init__(name = "coffee", price = 0.40)
     def description(self):
         return "A coffee, to stay awake."
 
 class Tea(HotBeverage):
-    name = "tea"
+    def __init__(self):
+        super().__init__("tea")
     # price is not neccessary
     def description(self):
         return "Just some hot water in a cup."
     
 class Chocolate(HotBeverage):
-    name = "chocolate"
-    price = 0.50
-
+    def __init__(self):
+        super().__init__("chocolate", 0.50)
     def description(self):
         return "Chocolate, sweet chocolate..."
 
 class Cappuccino(HotBeverage):
-    name = "cappuccino"
-    price = 0.45
-
+    def __init__(self):
+        super().__init__("cappuccino", 0.45)
     def description(self):
         return "Un po’ di Italia nella sua tazza!"
 
@@ -40,27 +38,17 @@ class Cappuccino(HotBeverage):
 if __name__== '__main__':
 
     hot_beverage = HotBeverage()
-    print("\nname : ", hot_beverage.name)
-    print("price : ", hot_beverage.price)
-    print("description : ", hot_beverage.description())
+    print(hot_beverage)
 
     coffee = Coffee()
-    print("\nname : ", coffee.name)
-    print("price : ", coffee.price)
-    print("description : ", coffee.description())
+    print(coffee)
 
     tea = Tea()
-    print("\nname : ", tea.name)
-    print("price : ", tea.price)
-    print("description : ", tea.description())
+    print(tea)
 
     chocolate = Chocolate()
-    print("\nname : ", chocolate.name)
-    print("price : ", chocolate.price)
-    print("description : ", chocolate.description())
+    print(chocolate)
 
     cappuccino = Cappuccino()
-    print("\nname : ", cappuccino.name)
-    print("price : ", cappuccino.price)
-    print("description : ", cappuccino.description())
+    print(cappuccino)
     
--- Django_0_OOB/ex03/machine.py
+++ Django_0_OOB/ex03/machine.py
@@ -1,11 +1,12 @@
-
+import random
 from beverages import HotBeverage
 
 
 class CoffeeMachine:
-    def __init__(self, cups, status="working"):
-        self.cups = cups
+    def __init__(self, name, status="working"):
+        self.name = name
         self.status = status
+        self.served_cups = 0
     
     class EmptyCup(HotBeverage):
         name = "empty cup"
@@ -18,10 +19,57 @@ class CoffeeMachine:
         def __init__(self):
             super().__init__("This coffee machine has to be repaired.")
     
-    # def repair():
+    def repair(self):
+        self.status = "working"
+        self.served_cups = 0
+        
 
-    def serve():
-        if random.randint(0, 1):
-            return CoffeeMachine.EmptyCup()
+    def serve(self, beverage):
+        if self.served_cups >= 10:
+            self.status = "broken"
+            raise CoffeeMachine.BrokenMachineException()
         else:
-            return Coffee()
+            self.served_cups += 1
+            if random.randint(0, 1):
+                return CoffeeMachine.EmptyCup()
+            else:
+                return beverage()
+
+
+def test_coffee_machine():
+
+    # Instantiate a coffee machine
+    coffee_machine = CoffeeMachine("The Amazing Coffee Machine")
+    print(coffee_machine.name)
+    print("Status : ", coffee_machine.status)
+    
+    # Serve 12 beverages
+    try:
+        for i in range(12):
+            result = coffee_machine.serve(HotBeverage)
+            print(f"Served Beverage: {result.name}")
+            print(f"Total served beverages: {coffee_machine.served_cups}")
+            print(result)
+    except CoffeeMachine.BrokenMachineException:
+        # Repair the coffee machine
+        print("The coffee machine is broken. Repairing...")
+        coffee_machine.repair()
+        print("The coffee machine has been repaired.")
+
+    # Serve 12 beverages again
+    try:
+        for i in range(12):
+            result = coffee_machine.serve(HotBeverage)
+            print(f"Served Beverage: {result.name}")
+            print(f"Total served beverages: {coffee_machine.served_cups}")
+            print(result)
+    except CoffeeMachine.BrokenMachineException:
+        # Repair the coffee machine
+        print("The coffee machine is broken. Repairing...")
+        coffee_machine.repair()
+        print("The coffee machine has been repaired.")
+
+
+
+if __name__== '__main__':
+    test_coffee_machine()
