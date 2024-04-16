1a943072f0a595fdcf60fd17a026622163f715d3
subrandt <susanneimnetz@hotmail.com>
2024-04-16 17:38:33 +0200

    reste a corriger h1 et img et les tests a passer

--- Django_0_OOB/ex04/elem.py
+++ Django_0_OOB/ex04/elem.py
@@ -8,12 +8,20 @@ class Text(str):
     Because directly using str class was too mainstream.
     """
 
-    def __str__(self):
+    def __str__(self, level=0):
         """
         Do you really need a comment to understand this method?..
         """
-        return super().__str__().replace('\n', '\n<br />\n')
+        s = super().__str__()
+        s = s.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;').replace('"', '&quot;')
+        return s
+    
 
+
+    def to_string(self, level=0):
+        s = self.__str__()
+        s = '  ' * level + s.replace('\n', '\n' + '  ' * level)
+        return s
 
 class Elem:
     """
@@ -30,22 +38,10 @@ class Elem:
         """
         self.tag = tag
         self.attr = attr
-        self.content = []
-        if content:
-            self.add_content(content)
+        self.content = [] if content is None else [content]
         self.tag_type = tag_type
-    
-    def add_content(self, content):
-        """
-        Add a content to the element.
-        """
-        if isinstance(content, Elem) or isinstance(content, Text):
-            self.content.append(content)
-        else:
-            raise self.ValidationError("Content must be an instance of Elem or Text")
 
-
-    def __str__(self):
+    def __str__(self, level=0):
         """
         The __str__() method will permit us to make a plain HTML representation
         of our elements.
@@ -53,11 +49,21 @@ class Elem:
         elements...).
         """
         if self.tag_type == 'double':
-            content = escape_html(self.__make_content())
-            result = f"<{self.tag}{self.__make_attr()}>{content}</{self.tag}>"
-        elif self.tag_type == 'simple':
-            result = f"<{self.tag}{self.__make_attr()} />"
-        return result
+            if isinstance(self.content, list):
+                content = '\n'.join(item.to_string(level + 1) if isinstance(item, (Elem, Text)) else str(item) for item in self.content)
+            elif isinstance(self.content, (Elem, Text)):
+                content = self.content.to_string(level + 1)
+            else:
+                content = str(self.content) if self.content else ''
+            indent = '  ' * level
+            inner_indent = '  ' * (level + 1)
+            content = f'\n{inner_indent}' + content + f'\n{indent}' if content else ''
+            return f"{indent}<{self.tag}>{content}{indent}</{self.tag}>"
+        else:
+            return f"{'  ' * level}<{self.tag} />"
+        
+    def to_string(self, level=0):
+        return self.__str__(level)
 
     def __make_attr(self):
         """
@@ -81,11 +87,11 @@ class Elem:
         return result
 
     def add_content(self, content):
-        if not Elem.check_type(content):
-            raise Elem.ValidationError
-        if type(content) == list:
-            self.content += [elem for elem in content if elem != Text('')]
-        elif content != Text(''):
+        # if not Elem.check_type(content):
+        #     raise Elem.ValidationError
+        # if type(content) == list:
+        #     self.content += [elem for elem in content if elem != Text('')]
+        # elif content != Text(''):
             self.content.append(content)
 
     @staticmethod
@@ -105,22 +111,12 @@ def create_html_output():
     """
     elem = Elem(tag='html', attr={})
     elem.add_content(Elem(tag='head', attr={},
-                          content=Elem(tag='title', attr={}, content=Text('Hello ground!'))))
+                          content=Elem(tag='title', attr={}, content=Text('"Hello ground!"'))))
     elem.add_content(Elem(tag='body', attr={},
-                          content=[Elem(tag='h1', attr={}, content=Text('Oh no, not again!')),
+                          content=[Elem(tag='h1', attr={}, content=Text('"Oh no, not again!"')),
                                    Elem(tag='img', attr={'src': 'http://i.imgur.com/pfp3T.jpg'}, tag_type='simple')]))
-    
-    with open('test.html', 'w') as file:
-        file.write(str(elem))
+
     print(elem)
-
-def escape_html(text):
-    """
-    Escape special characters in text to their HTML entities.
-    """
-    return text.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;').replace('"', '&quot;').replace("'", '&#39;')
-
-
 
 if __name__ == '__main__':
     create_html_output()
