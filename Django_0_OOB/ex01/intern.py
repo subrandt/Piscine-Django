f95cea13b4202e03021615904955df8a2e3eaa0f
subrandt <susanneimnetz@hotmail.com>
2024-04-21 18:05:55 +0200

    correction ex01

--- Django_0_OOB/ex01/intern.py
+++ Django_0_OOB/ex01/intern.py
@@ -15,27 +15,39 @@ class Coffee:
         def make_coffee(self):
             return self.Coffee()
 
+
 def test_intern():
     try:
+        # Create an instance of Coffee
         some_coffee = Coffee()
         print("Some coffee: ", some_coffee)
 
+        # Try to make coffee with Coffee instance, this should fail
+        try:
+            some_coffee.make_coffee()
+        except AttributeError:
+            print("Coffee class cannot make coffee")
+
+        # Create an instance of Intern
         Intern = Coffee.Intern()
         print("Intern: ", Intern)
 
-        
+        # Create an instance of Intern and make coffee, this should succeed
         nobody = Intern()
         print("Instantiation without name: ", nobody)
-        print("Make me a coffée, nobody! ", nobody.make_coffee())
+        print("Make me a coffee, nobody! ", nobody.make_coffee())
         mark = Intern("Mark")
         print("Instantiation with name: ", mark)
-        print("Make me a coffée, Mark! ", mark.make_coffee())
+        print("Make me a coffee, Mark! ", mark.make_coffee())
 
-        nobody.work()
-        
+        # Try to make Intern work, this should fail
+        try:
+            nobody.work()
+        except Exception as e:
+            print(e)
+
     except Exception as e:
         print(e)
-
 
 if __name__ == '__main__':
     test_intern()
