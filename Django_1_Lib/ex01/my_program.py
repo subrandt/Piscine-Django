f4b050e5355d499da830467c4c600ef0b9b4d18e
Susanne Brandt <susanne.brandt@scub.net>
2024-05-02 14:16:00 +0200

    my_script ok

--- Django_1_Lib/ex01/my_program.py
+++ Django_1_Lib/ex01/my_program.py
@@ -4,11 +4,19 @@ except ImportError:
     print("Module 'path' is not installed")
     Path = None
 
-# Create a folder and a file, write something in it, and display its content
-p = Path('my_directory')
-p.mkdir_p()
-f = p / 'my_file.txt'
-f.write_text('Hello, World!')
+if Path is not None:
+    # Create a folder and a file, write something in it, and display its content
+    folder = Path('my_directory')
+    folder.mkdir_p()
+    assert folder.exists(), "Failed to create directory"
 
-# Read and display the file content
-print(f.read_text())
+
+    filename = folder / 'my_file.txt'
+    filename.write_text('Hello, World!')
+    assert filename.exists(), "Failed to create file"
+
+
+    # Read and display the file content
+    print(filename.read_text())
+else:
+    print("Cannot execute the program because 'path' module is not installed")
