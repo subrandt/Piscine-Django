63fc9ba2f8576303650a077ba8c31ad4ad9cf1c8
subrandt <susanneimnetz@hotmail.com>
2024-05-14 14:56:45 +0200

    ex03 fini

--- Django_1_Base_Django/ex03/d04/ex03/views.py
+++ Django_1_Base_Django/ex03/d04/ex03/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
 def color_view(request):
-    colors = ['black', 'red', 'blue', 'green']
     shades = [hex(i)[2:].zfill(2) for i in range(0, 256, 5)]  # Generate 50 shades for each color
-    color_shades = {color: [(color + shade * (color != 'black')) for shade in shades] for color in colors}
-    return render(request, 'ex03/color_table.html', {'color_shades': color_shades})
+
+    # Correctly generate color shades
+    color_shades = {
+        'black': ['#' + s*3 for s in shades],
+        'red': ['#' + s + '0000' for s in shades],
+        'green': ['#00' + s + '00' for s in shades],
+        'blue': ['#0000' + s for s in shades],
+    }
+
+    colors = list(color_shades.keys())
+    shades = list(zip(*color_shades.values()))
+
+    return render(request, 'ex03/color_table.html', {'colors': colors, 'shades': shades})
