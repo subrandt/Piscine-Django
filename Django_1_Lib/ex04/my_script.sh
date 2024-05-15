6c6eb1971a007c9253e93e30fdc3957196f87890
subrandt <susanneimnetz@hotmail.com>
2024-05-15 22:17:49 +0200

    ajout droits d'ecriture script ex04

--- Django_1_Lib/ex04/my_script.sh
+++ Django_1_Lib/ex04/my_script.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 # Execute this script with:
+# Don't do that in shared folder!!!
 # source my_script.sh
 
 # Create a virtual environment
 python3 -m venv django_venv
 
+# Change permissions
+chmod -R 755 lib
+
 # Activate the virtual environment
-source django_venv/bin/activate
+django_venv/bin/activate
 
 # Install the dependencies
 pip install -r requirement.txt
-
