a814d8d3c9946adcdda184108a20c249fbe60ef6
subrandt <susanneimnetz@hotmail.com>
2024-05-19 22:47:15 +0200

    debut des installations sql mais ca capte pas a Arcachon

---
+++ Django_2_SQL/create_virtualenv.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Execute this script with:
+# source create_virtualenv.sh
+
+# Create a virtual environment
+python3 -m venv virtualenv
+
+# Activate the virtual environment
+source virtualenv/bin/activate
+
+# Install the dependencies
+pip install -r requirement.txt
+
