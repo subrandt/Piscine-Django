3aac3ed7e102e22331371ab1898f6fa0581bcff5
subrandt <susanneimnetz@hotmail.com>
2024-06-27 11:44:04 +0200

    ex01 fini

--- Django_3_Sessions/ex/tips_website/urls.py
+++ Django_3_Sessions/ex/tips_website/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('home/', lambda request: redirect('/', permanent=False)),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- Django_3_Sessions/ex/tips_website/views.py
+++ Django_3_Sessions/ex/tips_website/views.py
@@ -1,19 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.conf import settings
 import random
 from datetime import datetime, timedelta
+from django.contrib.auth import authenticate, login, logout
+from .forms import UserRegistrationForm, UserLoginForm
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+
 
 def home(request):
     current_time = datetime.now()
-    if 'username' in request.session and 'username_time' in request.session:
-        username_time = datetime.strptime(request.session['username_time'], '%Y-%m-%d %H:%M:%S.%f')
-        if current_time - username_time > timedelta(seconds=42):
-            # Les 42 secondes sont écoulées, mettre à jour le nom et l'horodatage
+    if request.user.is_authenticated:
+        username = request.user.username
+    else:
+        if 'username' in request.session and 'username_time' in request.session:
+            username_time = datetime.strptime(request.session['username_time'], '%Y-%m-%d %H:%M:%S.%f')
+            if current_time - username_time > timedelta(seconds=42):
+                request.session['username'] = random.choice(settings.NAMES)
+                request.session['username_time'] = current_time.strftime('%Y-%m-%d %H:%M:%S.%f')
+        else:
             request.session['username'] = random.choice(settings.NAMES)
             request.session['username_time'] = current_time.strftime('%Y-%m-%d %H:%M:%S.%f')
-    else:
-        # Aucun nom ou horodatage dans la session, les créer
-        request.session['username'] = random.choice(settings.NAMES)
-        request.session['username_time'] = current_time.strftime('%Y-%m-%d %H:%M:%S.%f')
+        username = request.session['username']
 
-    return render(request, 'home.html', {'username': request.session['username']})
+    return render(request, 'home.html', {'username': username})
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'register.html', {'form': form})
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                form.add_error(None, "Invalid username or password")
+    else:
+        form = UserLoginForm()
+    return render(request, 'login.html', {'form': form})
+
+@login_required(login_url='login/')
+def logout_view(request):
+    logout(request)
+    return redirect( reverse('login'))
