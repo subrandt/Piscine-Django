be298eae266040c5aea4fb0c259b2729261b27af
subrandt <susanneimnetz@hotmail.com>
2024-06-28 17:21:22 +0200

    setup website articles - tout reste a faire

---
+++ Django_3_Advanced/articles/website/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.shortcuts import redirect
+from django.contrib import admin
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('home/', lambda request: redirect('/', permanent=False)),
+    # path('register/', views.register, name='register'),
+    # path('login/', views.login_view, name='login'),
+    # path('logout/', views.logout_view, name='logout'),
+]
