11e840afb7a49c23b11a3ab5d792a4e3abd389bd
subrandt <susanneimnetz@hotmail.com>
2024-07-08 22:18:29 +0200

    que faut il faire ?

--- Django_3_Final/d09/account/views.py
+++ Django_3_Final/d09/account/views.py
@@ -1,13 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, logout
 from django.http import JsonResponse
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render
 
 def account_view(request):
 	if request.method == 'POST':
 		if 'logout' in request.POST:
 			logout(request)
 			return JsonResponse({'logged_out': True})
+		elif 'register' in request.POST:
+			form = UserCreationForm(request.POST)
+			if form.is_valid():
+				user = form.save()
+				login(request, user)
+				return JsonResponse({'registered': True, 'username': user.username})
+			else:
+				return JsonResponse({'errors': form.errors})
 		else:
 			form = AuthenticationForm(request, data=request.POST)
 			if form.is_valid():
@@ -15,7 +23,12 @@ def account_view(request):
 				login(request, user)
 				return JsonResponse({'logged_in': True, 'username': user.username})
 			else:
-				return JsonResponse({'errors': form.errors})
+				# Ici, on gère l'échec de la connexion
+				# Vous pouvez ajuster le message ou la logique selon vos besoins
+				return JsonResponse({
+					'errors': form.errors,
+					'register_suggestion': 'Login failed. If you do not have an account, please register.'
+				})
 	else:
 		if request.user.is_authenticated:
 			return render(request, 'logged_in.html', {'username': request.user.username})
