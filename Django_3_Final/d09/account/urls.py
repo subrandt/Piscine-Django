d81252f0c3658bbc1cfa215e41c492665a1cda02
subrandt <susanneimnetz@hotmail.com>
2024-09-26 19:51:26 +0200

    routes de merde

--- Django_3_Final/d09/account/urls.py
+++ Django_3_Final/d09/account/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
 	path('', views.account_view, name='account_view'),
-    path('account', views.account_view, name='account_view'),
+    # path('account/', views.account_view, name='account_view'),
     path('login/', views.loginUser),
 	path('logout/', views.logoutUser),
 	path('chatrooms/', include('chat.urls')),
